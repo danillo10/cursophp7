97a2378a52036934286a2d81158867c980029357
Danillo Leão Lopes <danillo_10_@hotmail.com>
2017-11-06 14:14:06 -0300

    Curso em andamento

---
+++ seguranca/exemplo-04.php
@@ -0,0 +1,9 @@
+<script src='https://www.google.com/recaptcha/api.js'></script>
+
+<form action="cadastrar.php" method="POST">
+    
+    <div class="g-recaptcha" data-sitekey="6LcwQTcUAAAAAMbKBeirzwZp3EYbJ4Tn6mt-Hnvs"></div>
+    <input type="email" name="inputEmail">
+    <button type="submit">Enviar</button>
+
+</form>
