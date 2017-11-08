c954f3fbb338649d8cfd390d53a1bb726d4ba75a
Danillo Leão Lopes <danillo_10_@hotmail.com>
2017-11-08 15:25:44 -0300

    Curso em andamento

--- e-commerce/index.php
+++ e-commerce/index.php
@@ -293,7 +293,7 @@ $app->get("/admin/categories/:idcategory", function($idcategory) {
 $app->post("/admin/categories/:idcategory", function($idcategory) {
 
 	User::verifyLogin();
-	
+
 	$category = new Category();
 
 	$category->get((int)$idcategory);
@@ -304,6 +304,21 @@ $app->post("/admin/categories/:idcategory", function($idcategory) {
 
 	header("Location: /sites/trunk/danillo10/e-commerce/admin/categories");
 	exit;
+
+});
+
+$app->get("/categories/:idcategory", function($idcategory) {
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page = new Page();
+
+	$page->setTpl("category", array(
+		"category"=>$category->getValues(),
+		"products"=>[]
+	));
 
 });
 
