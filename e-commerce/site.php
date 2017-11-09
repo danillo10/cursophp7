ebec795dd36592f97f8ae58f294212e784cd4375
Danillo Leão Lopes <danillo_10_@hotmail.com>
2017-11-09 15:35:29 -0300

    Curso em andamento

--- e-commerce/site.php
+++ e-commerce/site.php
@@ -1,14 +1,34 @@
 <?php 
 
 use \Hcode\Page;
+use \Hcode\Model\Products;
+use \Hcode\Model\Category;
 
 $app->get('/', function() {
+
+	$products = Products::listAll();
     
 	$page = new Page();
 
-	$page->setTpl("index");
+	$page->setTpl("index",array(
+		"products"=>Products::checkList($products)
+	));
 
 });
 
+$app->get("/categories/:idcategory", function($idcategory) {
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page = new Page();
+
+	$page->setTpl("category", array(
+		"category"=>$category->getValues(),
+		"products"=>Products::checkList($category->getProducts())
+	));
+
+});
 
  ?>
